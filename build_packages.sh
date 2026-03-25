b33738db46699136e43d78efc0a1965c90056ce6
Andrew Hexen <SyberHexen@gmail.com>
2026-03-24 22:44:01 -0700

    build: Update script for nv-settings

--- build_packages.sh
+++ build_packages.sh
@@ -3,3 +3,13 @@
 dpkg-source --before-build ./
 debuild -b
 dpkg-buildpackage -b --host-arch=i386 -d
+cd ./nvidia-settings
+make
+while true; do
+    read -p "Do you wish to install nvidia-settings? " yn
+    case $yn in
+        [Yy]* ) sudo make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
